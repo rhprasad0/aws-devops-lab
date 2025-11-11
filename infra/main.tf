d4c36404e7355e6bffa7f79832fad3126053eb61
rhprasad0 <rhprasad@outlook.com>
2025-11-11 18:58:17 -0500

    Added public & private subnets

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
@@ -28,5 +28,31 @@ provider "aws" {
       created_at = "2025-11-11"
       ttl_hours  = var.ttl_hours
     }
+  }
+}
+
+# VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  
+  name = "${var.env}-vpc"
+  cidr = "10.0.0.0/16"
+  
+  azs             = ["${var.region}a", "${var.region}b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+  
+  enable_nat_gateway = false  # Save $64/month
+  enable_vpn_gateway = false
+  
+  # EKS subnet tags
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.env}-eks" = "shared"
+  }
+  
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.env}-eks" = "shared"
   }
 }
